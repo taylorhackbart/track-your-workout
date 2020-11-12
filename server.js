49574cecb638a2628b1b55809a6b5fce99aeb152
Taylor <taylor.hackbart@du.edu>
2020-11-12 13:55:14 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose= require("mongoose");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tracker"), { useNewUrlParser: true, useFindAndModify: false}
+
+app.use(require("./routes/api-routes.js"))
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});

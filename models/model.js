d607f6eb91c7ae572f43362af6b0bcd3b2856d95
Taylor <taylor.hackbart@du.edu>
2020-11-13 12:21:47 -0800

    model message

--- models/model.js
+++ models/model.js
@@ -35,5 +35,5 @@ const workoutSchema = new Schema ({
     }
   ]
 })
-const Workout = mongo.define("Workout", workoutSchema)
+const Workout = mongoose.model("Workout", workoutSchema)
 module.exports = Workout

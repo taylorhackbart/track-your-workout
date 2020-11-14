500cdeb39ad5216ac0fbb841ab2ebfc6c19de417
Taylor <taylor.hackbart@du.edu>
2020-11-14 14:03:50 -0800

    deploying

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout"), 
-{ useNewUrlParser: true, useFindAndModify: false}
+{ useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false}
 
 var apiRoute = (require("./routes/api-routes.js"))
 var htmlRoute = (require("./routes/html-routes.js"))
